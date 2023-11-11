ba27f35a019ec090752e1b7edd058780671aecf6
dmtr636 <dmtr636@gmail.com>
2023-11-11 17:52:17 +0300

    Some changes

--- src/shared/constants/config.ts
+++ src/shared/constants/config.ts
@@ -1,5 +1,5 @@
-const DEVELOPMENT_DOMAIN = "http://localhost:8080";
-// const DEVELOPMENT_DOMAIN = "https://hack-kydas.ru";
+// const DEVELOPMENT_DOMAIN = "http://localhost:8080";
+const DEVELOPMENT_DOMAIN = "https://hack-kydas.ru";
 
 const PRODUCTION_DOMAIN = "https://hack-kydas.ru";
 
